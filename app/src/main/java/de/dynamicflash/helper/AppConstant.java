29b5dadfa50265f80cbc7759ff0b3d21fcfd6c0e
Eric Funk <psytraxx@gmail.com>
2019-09-11 23:11:32 +0200

    fix api url

--- app/src/main/java/de/dynamicflash/helper/AppConstant.java
+++ app/src/main/java/de/dynamicflash/helper/AppConstant.java
@@ -12,13 +12,13 @@ public class AppConstant {
 
     public static final String BASE_URL = "https://www.dynamicflash.de";
 
-    public static final String GALLERY_LIST_JSON =  "/json/galleries?currentpage=%s&maxresults=%s";
+    public static final String GALLERY_LIST_JSON =  "/api/galleries?currentpage=%s&maxresults=%s";
 
-    public static final String PROJECT_LIST_JSON =  "/json/projects";
+    public static final String PROJECT_LIST_JSON =  "/api/projects";
 
     /**
      * http://de.dynamicflash.de/json/album/BangkokBaliLombok
      */
-    public static final String ALBUM_LIST_JSON =  "/json/album/%s";
+    public static final String ALBUM_LIST_JSON =  "/api/album/%s";
 
 }
