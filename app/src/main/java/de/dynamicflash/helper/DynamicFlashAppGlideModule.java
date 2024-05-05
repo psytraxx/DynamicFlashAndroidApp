9818badcb9720fa27cb258bfab34f798575b82f8
eric <psytraxx@gmail.com>
2024-05-05 09:11:29 +0200

    placeholder and timeout

--- app/src/main/java/de/dynamicflash/helper/DynamicFlashAppGlideModule.java
+++ app/src/main/java/de/dynamicflash/helper/DynamicFlashAppGlideModule.java
@@ -1,6 +1,8 @@
 package de.dynamicflash.helper;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 
 import androidx.annotation.NonNull;
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
@@ -9,11 +11,13 @@ import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
 
 @GlideModule
 public class DynamicFlashAppGlideModule extends AppGlideModule {
 
     public static final String EXTRA_IMAGE_URL_PARAMS = "&fm=avif";
+    private static final int GLIDE_REQUEST_TIMOUT_MS = 30000;
 
     @Override
     public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
@@ -25,6 +29,7 @@ public class DynamicFlashAppGlideModule extends AppGlideModule {
         int cacheSize = 30 * 1024 * 1024; // 10 MiB
 
         builder.setDiskCache(new InternalCacheDiskCacheFactory(context, cacheSize));
+        builder.setDefaultRequestOptions(new RequestOptions().timeout(GLIDE_REQUEST_TIMOUT_MS).placeholder(circularProgressDrawable));
         super.applyOptions(context, builder);
     }
 
