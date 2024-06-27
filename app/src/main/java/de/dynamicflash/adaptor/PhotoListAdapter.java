44bd013fe773e3bec16b7b7d72f0366b4bfd6f0e
eric <psytraxx@gmail.com>
2024-06-27 21:42:28 +0200

    remove dependabot.yml

--- app/src/main/java/de/dynamicflash/adaptor/PhotoListAdapter.java
+++ app/src/main/java/de/dynamicflash/adaptor/PhotoListAdapter.java
@@ -3,7 +3,6 @@ package de.dynamicflash.adaptor;
 import static de.dynamicflash.helper.DynamicFlashAppGlideModule.EXTRA_IMAGE_URL_PARAMS;
 import static de.dynamicflash.helper.DynamicFlashAppGlideModule.IMAGE_BASE_URL;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,32 +34,38 @@ public class PhotoListAdapter extends ArrayAdapter<Photo> {
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
 
-        final ViewHolder holder;
+        ViewHolder holder; // No need to initialize here
+
         if (convertView == null) {
-            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext()); // More efficient way to get LayoutInflater
             convertView = inflater.inflate(resource, parent, false);
-            holder = new ViewHolder();
-            holder.image = convertView.findViewById(R.id.imageID);
+            holder = new ViewHolder(convertView); // Initialize ViewHolder directly
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        if (getCount() > position) {
+        // Check position validity before accessing item
+        if (position >= 0 && position < getCount()) {
             Photo item = getItem(position);
-            assert item != null;
+            if (item != null) { // Null check for item
+                String uri = IMAGE_BASE_URL + item.getImage() + "?w=320&h=240&dpr=2&fit=cover" + EXTRA_IMAGE_URL_PARAMS;
 
-            final String uri = IMAGE_BASE_URL + item.getImage() + "?w=320&h=240&dpr=2&fit=cover" + EXTRA_IMAGE_URL_PARAMS;
-
-            Glide.with(context)
-                    .load(uri)
-                    .into(holder.image);
+                Glide.with(context)
+                        .load(uri)
+                        .into(holder.image);
+            }
         }
 
         return convertView;
     }
 
+    // ViewHolder class with constructor for view initialization
     static class ViewHolder {
         ImageView image;
+
+        ViewHolder(View view) {
+            image = view.findViewById(R.id.imageID);
+        }
     }
 }
